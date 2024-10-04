649dd5c57cbd6b295a9a2cb80d4485a6eb0c7492
metin.koyuncu <metin.koyuncu@METINKOYUNCU>
2024-10-04 23:53:00 +0300

    checkbox kontrol

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Select from './components/select/select';
@@ -6,13 +6,27 @@ import SelectDemo from './components/select/SelectDemo';
 import RadioDemo from './components/radio/radioDemo';
 import FileDemo from './components/file/fileDemo';
 import 'react-select-search/style.css'
+import  Checkbox  from "react-woodpecker";
 
 function App() {
-  
+  const [checke,setChecke] = useState(false);
+
+  const x = ()=>{
+    console.log(checke);
+    setChecke(!checke)
+  }
 
   return (
     <div className="App">
- 
+      <Checkbox
+        label="Primary"
+        checked = {true}
+        onChange = {()=>x()}
+        colorUnchecked="Primary"
+        colorChecked="red"
+        variant="primary"
+        initialColor="primary"
+      />
       <SelectDemo/>
       
       <Select/>
